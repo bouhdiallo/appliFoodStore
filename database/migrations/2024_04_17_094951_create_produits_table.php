1e3fa4d0637dbbaea0332d5a87ff0f914c53ac7e
Bouhdiallo <bouhdiallo22@gmail.com>
2024-04-17 10:21:29 +0000

    mise en place des models controllers et etablissement de la base de donnees

---
+++ database/migrations/2024_04_17_094951_create_produits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use App\Models\Fournisseur;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produits', function (Blueprint $table) {
+            $table->id();
+            $table->string('nom_produit');
+            $table->float('prix');
+            $table->string('description');
+            $table->integer('qte_en_stock');
+            $table->foreignIdFor(Fournisseur::class)->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produits');
+    }
+};
---
+++ database/migrations/2024_04_17_095634_create_factures_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Client;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('factures', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date');
+            $table->float('montant');
+            $table->foreignIdFor(Client::class)->nullable()->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('factures');
+    }
+};
